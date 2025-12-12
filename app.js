76e742fc2b2b5d2acccfc5f2aa10b2348b29ef88
Aluno <lambgust@gmail.com>
2025-12-11 21:43:30 -0300

    updade package.json

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ require('dotenv').config();
 const {produtoRoutes} = require("./src/routes/produtoRoutes");
 const {clienteRoutes} = require("./src/routes/clienteRoutes");
 
-
-
-
 const PORT = process.env.PORT;
 
 app.use(express.json());// configurando o midleway para aceitar o json
