b0554e1e4f0bc0191d6e3d7f4c668750a05120bb
Aluno <lambgust@gmail.com>
2025-12-12 21:37:44 -0300

    fix test

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,29 +1,30 @@
-const jwt = require ("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 const verify = {
-    cliente: async (req,res,next) => {
-        
+    cliente: async (req, res, next) => {
+
         try {
 
-           const {token} = req.cookies;
+            const { token } = req.cookies;
 
 
-          const decoded = jwt.verify(token,process.env.JWP_SECRET);
-            
-        if(decoded.tipoUsuario || decoded.tipoUsuario !=="cliente"){
-             return res.status(403).json({error:"Acesso permitido somente para Clientes"});
+            const decoded = jwt.verify(token, process.env.JWP_SECRET);
+            console.log(decoded);
 
-           }
+            if (decoded.tipoUsuario || decoded.tipoUsuario !== "cliente") {
+                return res.status(403).json({ error: "Acesso permitido somente para Clientes" });
+
+            }
 
             next();
 
         } catch (error) {
-            return res.status(401).json({error:"Token invalido ou expirado"});
+            return res.status(401).json({ error: "Token invalido ou expirado" });
 
-            
+
         }
     }
 
 }
 
-module.exports = {verify}
+module.exports = { verify }
