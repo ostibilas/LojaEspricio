c0eb113cdf5841ce536c96793f253d1b1fa2a246
Aluno <lambgust@gmail.com>
2025-12-12 21:47:20 -0300

    fix test

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,6 +9,8 @@ const verify = {
 
 
             const decoded = jwt.verify(token, process.env.JWP_SECRET);
+            console.log("IF:" + decoded.tipoUsuario || decoded.tipoUsuario !== "cliente");
+            console.log(decoded.tipoUsuario);
             console.log(decoded.tipoUsuario !== "cliente");
 
             if (decoded.tipoUsuario || decoded.tipoUsuario !== "cliente") {
